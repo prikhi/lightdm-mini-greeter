62b699c1d9e104c8822867c0d9d5674466a496b5
Pavan Rikhi <pavan.rikhi@gmail.com>
2018-01-22 13:43:53 -0500

    Make Default User Generic
    Switch the pre-existing `user` value in the configuration file from
    `prikhi` to `CHANGE_ME`.

--- src/config.c
+++ src/config.c
@@ -31,6 +31,9 @@ Config *initialize_config(void)
     // Parse values from the keyfile into a Config.
     config->login_user = g_key_file_get_string(
         keyfile, "greeter", "user", NULL);
+    if (strcmp(config->login_user, "CHANGE_ME") == 0) {
+        g_message("User configuration value is unchanged.");
+    }
     config->show_password_label = g_key_file_get_boolean(
         keyfile, "greeter", "show-password-label", NULL);
     config->password_label_text = g_key_file_get_string(
